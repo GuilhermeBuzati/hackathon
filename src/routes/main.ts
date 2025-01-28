3d6a0f4a5cf9c3330da2a50ca0882b70f921b318
Guilherme Buzati <guilhermeleopoldinoo@gmail.com>
2025-01-27 21:42:57 -0300

    feat: Pasta server criada

--- src/routes/main.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-const mainRouter = express.Router();
-
-/* GET home page. */
-mainRouter.get('/', (req: Request, res: Response) => {
-  res.send('respond with a resource');
-});
-
-export default mainRouter;
